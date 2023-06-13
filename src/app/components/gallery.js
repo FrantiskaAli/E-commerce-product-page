5e0dff9bc2e766fb20dcf6c63e55c872bd103071
FrantiskaAli <tiskarechk@gmail.com>
2023-06-13 12:10:30 +0100

    basic hover effects set with tailwind on the main page

--- src/app/components/gallery.js
+++ src/app/components/gallery.js
@@ -4,13 +4,14 @@ export default function Gallery() {
     return (
         <section className="w-2/4 px-14 flex flex-col pt-0 justify-between">
             <article className="w-full h-full relative mb-4">
-                <Image src="/image-product-1.jpg" layout="fill" className="rounded-2xl" />
+                <Image src="/image-product-1.jpg" layout="fill" className="rounded-2xl cursor-pointer" />
             </article>
             <article className="flex justify-between mt-4">
-                <Image src="/image-product-1-thumbnail.jpg" width={80} height={80} alt="Product Image" className="rounded-lg" />
-                <Image src="/image-product-1-thumbnail.jpg" width={80} height={80} alt="Product Image" className="rounded-lg" />
-                <Image src="/image-product-1-thumbnail.jpg" width={80} height={80} alt="Product Image" className="rounded-lg" />
-                <Image src="/image-product-1-thumbnail.jpg" width={80} height={80} alt="Product Image" className="rounded-lg" />
+                <Image src="/image-product-1-thumbnail.jpg" width={80} height={80} alt="Product Image" className="rounded-lg cursor-pointer hover:opacity-50" />
+                <Image src="/image-product-1-thumbnail.jpg" width={80} height={80} alt="Product Image" className="rounded-lg cursor-pointer hover:opacity-50" />
+                <Image src="/image-product-1-thumbnail.jpg" width={80} height={80} alt="Product Image" className="rounded-lg cursor-pointer hover:opacity-50" />
+                <Image src="/image-product-1-thumbnail.jpg" width={80} height={80} alt="Product Image" className="rounded-lg cursor-pointer hover:opacity-50" />
+
             </article>
         </section>
     )
