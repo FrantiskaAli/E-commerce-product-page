66b1890b24c086afaf76b7ab87b8af7778c13cef
FrantiskaAli <tiskarechk@gmail.com>
2023-06-14 23:41:52 +0100

    working on design

--- src/app/components/cart.js
+++ src/app/components/cart.js
@@ -15,31 +15,32 @@ useEffect(()=>{
     }
 
     const emptyCart = (
-        <section style={{ top: position, right: '5rem' }} className="h-50 w-96 text-center border-2 border-solid p-4 border-gray-500 absolute">
-            <h3>Cart</h3>
-            <hr></hr>
-            Your cart is empty
+        <section className="h-1/4 w-1/4 top-16 mt-12 right-20 text-center shadow-lg absolute bg-white rounded-2xl">
+            <h3 className="text-left font-bold p-4">Cart</h3>
+            <article className="border-t-2 h-2/3 border-gray-200 flex items-center justify-center font-bold text-gray-400">
+           <p>Your cart is empty</p> 
+            </article>
         </section>
     )
 
     const fullCart = (
-        <section style={{ top: position, right: '5rem' }} className="h-50 w-96 text-center border-2 border-solid p-4 border-gray-500 absolute">
-            <h3>Cart</h3>
+        <section style={{ top: position, right: '5rem' }} className="h-1/4 w-1/4 top-22 right-20 text-center p-4 shadow-lg absolute bg-white">
+            <h3 className="text-left font-bold p-4">Cart</h3>
             <hr></hr>
             <section>
-                <article className="flex">
-                    <Image src="/image-product-1-thumbnail.jpg" width={40} height={40} />
-                    <p>Fall Limited Edition Sneakers <br /> $125.00 x {cart} ${125.00 * cart} </p>
-                    <svg onClick={() => remove()} width="14" height="16" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"><defs><path d="M0 2.625V1.75C0 1.334.334 1 .75 1h3.5l.294-.584A.741.741 0 0 1 5.213 0h3.571a.75.75 0 0 1 .672.416L9.75 1h3.5c.416 0 .75.334.75.75v.875a.376.376 0 0 1-.375.375H.375A.376.376 0 0 1 0 2.625Zm13 1.75V14.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 1 14.5V4.375C1 4.169 1.169 4 1.375 4h11.25c.206 0 .375.169.375.375ZM4.5 6.5c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Z" id="a" /></defs><use fill="#C3CAD9" fillRule="nonzero" xlinkHref="#a" /></svg>
+                <article className="flex items-center p-4 ">
+                    <Image src="/image-product-1-thumbnail.jpg" width={50} height={50} alt="thumbnail of the product" className="rounded" />
+                    <p className="text-gray-400 text-left px-4">Fall Limited Edition Sneakers <br /> $125.00 x {cart} ${125.00 * cart} </p>
+                    <svg className="hover:brightness-50 hover:cursor-pointer" onClick={() => remove()} width="14" height="16" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"><defs><path d="M0 2.625V1.75C0 1.334.334 1 .75 1h3.5l.294-.584A.741.741 0 0 1 5.213 0h3.571a.75.75 0 0 1 .672.416L9.75 1h3.5c.416 0 .75.334.75.75v.875a.376.376 0 0 1-.375.375H.375A.376.376 0 0 1 0 2.625Zm13 1.75V14.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 1 14.5V4.375C1 4.169 1.169 4 1.375 4h11.25c.206 0 .375.169.375.375ZM4.5 6.5c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Z" id="a" /></defs><use fill="#C3CAD9" fillRule="nonzero" xlinkHref="#a" /></svg>
                 </article>
-                <button>Checkout</button>
+                <button className="bg-yellow-400 w-full rounded-lg py-4 my-2 hover:bg-yellow-600 hover:cursor-pointer  text-white font-bold ">Checkout</button>
             </section>
         </section>
     )
 
     if (cart > 0) {
         return fullCart
-    } else return emptyCart
+   } else return emptyCart
 
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -9,56 +9,53 @@ import GalleryActive from "./components/galleryActive";
 export default function Home() {
 
   const [galleryDisplay, setGalleryDisplay] = useState({ //this is the state of the pop up gallery and initial picture
-    open: false, 
-    display: 1 
+    open: false,
+    display: 1
   })
 
   const [cart, setCart] = useState({//this sets all the information I need for correct display and count in the cart element
     count: 0,
     display: false,
     info: 0,
-    position:0
+    position: 0
   })
 
 
   const plus = () => {//function to add items
     const current = cart.count
-    setCart({...cart, count: current + 1})
+    setCart({ ...cart, count: current + 1 })
     console.log("function executed")
   }
 
   const minus = () => { //function to subtract items
     const current = cart.count
     if (current > 0)
-    setCart({...cart, count: current - 1})
+      setCart({ ...cart, count: current - 1 })
   }
- 
+
   const showCart = (e) => { //function to display cart element and position it under cart's icon
-   cart.display ? setCart({...cart, display: false,position: e.target.offsetHeight + 10}) : setCart({...cart, display: true,position: e.target.offsetHeight + 10})
+    cart.display ? setCart({ ...cart, display: false, position: e.target.parentElement.offsetHeight - 10}) : setCart({ ...cart, display: true, position: e.target.parentElement.offsetHeight + 10 })
   }
 
   const onSubmit = () => { //function to add counted items into the cart
     const current = cart.count + cart.info;
-    setCart( {...cart,count:0, info:current });
+    setCart({ ...cart, count: 0, info: current });
   }
 
   const openGal = (number) => {//function to open gallery view
     setGalleryDisplay({ open: true, display: number })
   }
- const closeGal = (number)=>{
-  setGalleryDisplay({...galleryDisplay, open: false, display: number})
- }
+  const closeGal = (number) => {//this function closes the gallery and outlines last seen image in the gallery
+    setGalleryDisplay({ ...galleryDisplay, open: false, display: number })
+  }
 
   return (
-    <main className={galleryDisplay.open ? "w-screen h-screen overflow-hidden px-36" : "z-10 px-36"}>
-      <Navbar cartShow={showCart} />
+    <main className={galleryDisplay.open ? "w-screen h-screen overflow-hidden px-36" : "w-screen h-screen px-44"}>
+      <Navbar cartShow={showCart} cartActive={cart.display} />
       {cart.display && <Cart position={cart.position} cartInfo={cart.info} />}
-
-
-
       <section className="flex basis-full pt-20 pb-4 h-full space-between">
         {galleryDisplay.open && <GalleryActive close={closeGal} begin={galleryDisplay.display} />}
-        <Gallery openGal={openGal} lastSeen={galleryDisplay.display}/>
+        <Gallery openGal={openGal} lastSeen={galleryDisplay.display} />
         <Info onPlus={() => plus()} onMinus={() => minus()} cart={cart.count} addToCart={() => onSubmit()} />
       </section>
 
